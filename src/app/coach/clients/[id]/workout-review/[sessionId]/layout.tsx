b0c32dd5db2b28b03dc9448e0809d78b41ad30b1
Marcel_Allam <marcel.f.allam@gmail.com>
2026-06-16 14:18:09 +0100

    Make workout review layout compatible with async route params

--- src/app/coach/clients/[id]/workout-review/[sessionId]/layout.tsx
+++ src/app/coach/clients/[id]/workout-review/[sessionId]/layout.tsx
@@ -3,17 +3,19 @@ import { WorkoutFlagsLoader } from '@/components/coach/workout-flags-loader';
 
 type WorkoutReviewLayoutProps = {
   children: ReactNode;
-  params: {
+  params: Promise<{
     id: string;
     sessionId: string;
-  };
+  }>;
 };
 
-export default function WorkoutReviewLayout({ children, params }: WorkoutReviewLayoutProps) {
+export default async function WorkoutReviewLayout(props: WorkoutReviewLayoutProps) {
+  const routeParams = await props.params;
+
   return (
     <>
-      <WorkoutFlagsLoader clientId={params.id} sessionId={params.sessionId} />
-      {children}
+      <WorkoutFlagsLoader clientId={routeParams.id} sessionId={routeParams.sessionId} />
+      {props.children}
     </>
   );
 }
