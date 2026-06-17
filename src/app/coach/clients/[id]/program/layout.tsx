12931368831dfd6069ffa0a9c1e1d8edae3963dc
Marcel_Allam <marcel.f.allam@gmail.com>
2026-06-17 19:18:07 +0100

    Mount legacy template remover

--- src/app/coach/clients/[id]/program/layout.tsx
+++ src/app/coach/clients/[id]/program/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from 'react';
 import { AssignFromLibraryPanel } from '@/components/coach/assign-from-library-panel';
 import { ProgramWorkoutExercisePreviews } from '@/components/coach/program-workout-exercise-previews';
+import { RemoveLegacyTemplateBuilder } from '@/components/coach/remove-legacy-template-builder';
 
 type ProgramLayoutProps = {
   children: ReactNode;
@@ -12,6 +13,7 @@ export default function ProgramLayout({ children }: ProgramLayoutProps) {
       {children}
       <AssignFromLibraryPanel />
       <ProgramWorkoutExercisePreviews />
+      <RemoveLegacyTemplateBuilder />
     </>
   );
 }
