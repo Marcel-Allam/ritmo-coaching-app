b0e043608160c6c365ace6dbbaceaa0d577b0ab8
Marcel_Allam <marcel.f.allam@gmail.com>
2026-06-18 06:41:36 +0100

    Remove legacy programme layout helpers

--- src/app/coach/clients/[id]/program/layout.tsx
+++ src/app/coach/clients/[id]/program/layout.tsx
@@ -1,7 +1,5 @@
 import type { ReactNode } from 'react';
 import { AssignFromLibraryPanel } from '@/components/coach/assign-from-library-panel';
-import { ProgramWorkoutExercisePreviews } from '@/components/coach/program-workout-exercise-previews';
-import { RemoveLegacyTemplateBuilder } from '@/components/coach/remove-legacy-template-builder';
 
 type ProgramLayoutProps = {
   children: ReactNode;
@@ -12,8 +10,6 @@ export default function ProgramLayout({ children }: ProgramLayoutProps) {
     <>
       {children}
       <AssignFromLibraryPanel />
-      <ProgramWorkoutExercisePreviews />
-      <RemoveLegacyTemplateBuilder />
     </>
   );
 }
